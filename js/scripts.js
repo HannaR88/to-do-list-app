e63d8d689534968b941d50b782a338d07a9254a7
hanna <hanna-881@hotmail.com>
2023-09-04 12:07:23 -0400

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,43 @@
-function newItem() {
 
-  let li = $('<li></li>');
-  let inputValue = $('#input').val();
-  li.append(inputValue);
+function newItem(){
 
-  if (inputValue === '') {
-      alert("You must write something!");
+  // 1.  Adding a new item using jQuery:
+
+   let list = $('#list');
+   let inputValue = $('#input').val();
+   let li = $('<li></li>');
+   li.append(inputValue);
+
+   if (inputValue === '') {
+    alert("Please type something in the box before clicking ADD.");
   } else {
-      $('#list').append(li);
+    list.append(li);
   }
+
+  //2. Crossing out an item using jQuery:
 
   function crossOut() {
-      li.toggleClass("strike");
-  }
+		li.toggleClass('strike');
+	}
 
-  li.on("dblclick", function crossOut() {
-      li.toggleClass("strike");
-  });
+	li.on("dblclick", function crossOut() {
+		li.toggleClass('strike');
+	});
+  
+
+  //3. Adding the delete button in jQuery: 
 
   let crossOutButton = $('<crossOutButton></crossOutButton>');
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
-
   crossOutButton.on("click", deleteListItem);
-  function deleteListItem() {
-      li.addClass("delete");
-  }
+  function deleteListItem(){
+ 		li.addClass("delete");
+ 	}
+
+
+  // 4. Reordering the items in jQuery: 
+
   $('#list').sortable();
+
 }
